ab0fe833298d8aacdcad7b6a000bb8a7fdcb42c5
Mekatrol <Mekatrol@github.com>
2024-04-03 07:40:10 +1100

    baseline build

--- docker/image_processing_server/app/main.py
+++ docker/image_processing_server/app/main.py
@@ -9,25 +9,27 @@ from pycoral.utils.edgetpu import make_interpreter
 
 app = FastAPI()
 
-labels = read_label_file('/server/app/models/coco_labels.txt')
-interpreter = make_interpreter('/server/app/models/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite')
-interpreter.allocate_tensors()
-
-image = Image.open('/server/app/data/grace_hopper.bmp')
-_, scale = common.set_resized_input(interpreter, image.size, lambda size: image.resize(size, Image.LANCZOS))
-
-interpreter.invoke()
-objs = detect.get_objects(interpreter, 0.4, scale)
-
-detected = ''
-if not objs:
-    detected = 'No objects detected'
-else:
-    detected = f'{objs}'
-
 @app.get("/")
 def read_root():
-    return {"detect": detected}
+    labels = read_label_file('/server/app/models/coco_labels.txt')
+    interpreter = make_interpreter('/server/app/models/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite')
+    interpreter.allocate_tensors()
+    
+    image = Image.open('/server/app/data/grace_hopper.bmp')
+    _, scale = common.set_resized_input(interpreter, image.size, lambda size: image.resize(size, Image.LANCZOS))
+    
+    interpreter.invoke()
+    objs = detect.get_objects(interpreter, 0.4, scale)
+    
+    detected = ''
+    if not objs:
+        detected = 'No objects detected'
+    else:
+        detected = ''
+        for obj in objs:
+            detected += f'label: {labels.get(obj.id, obj.id)}, id: {obj.id}, score: {obj.score}, bbox: {obj.bbox}\n'
+
+    return {"detected": detected}
 
 
 @app.get("/items/{item_id}")
